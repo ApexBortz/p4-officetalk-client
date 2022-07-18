aed446bebc6af62ea76f5a3688bf95851ea96c5a
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-07-17 21:31:07 -0700

    Socket connection provider made

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import Dashboard from './components/Dashboard'
 import useLocalStorage from './hooks/useLocalStorage'
 import { ContactProvider } from './contexts/ContactProvider'
 import { ConversationProvider } from './contexts/ConversationProvider'
+import { SocketConnection } from './contexts/SocketConnection'
 
 const App = () => {
 
   // local storage of user id/username
   const [ id, setId ] = useLocalStorage('id')
 
-  // dashboard function to provide both contexts & render dashboard
+  // dashboard function to pass in contexts and socket connection
   const dashboardFunction = () => {
     return(
-      <ContactProvider>
-        <ConversationProvider id={id} >
-          <Dashboard id={id} />
-        </ConversationProvider>
-      </ContactProvider>
+      // provides socket connection
+      // provides contacts from context
+      // provides conversations from context
+      // renders dashboard
+      <SocketConnection id={id}>
+        <ContactProvider>
+          <ConversationProvider id={id} >
+            <Dashboard id={id} />
+          </ConversationProvider>
+        </ContactProvider>
+      </SocketConnection>
+
     )
   }
 
+  // ternary to render login if no id - rest of app if id
   return (
     <div className='App'>
 
--- src/contexts/SocketConnection.jsx
+++ src/contexts/SocketConnection.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { useContext } from 'react'
+import io from 'socket.io-client'
 
 const SocketContext = React.createContext()
 
@@ -7,8 +10,23 @@ export function useSocket(){
     return useContext(SocketContext)
 }
 
-export function SocketConnection() {
+export function SocketConnection({ id, children }) {
+
+    const [ socket, setSocket ] = useState()
+
+    // create socket when new page loadsor if there is an id change
+    useEffect(() => {
+        const newSocket = io('http://localhost:7000', { query: {id} })
+
+        setSocket(newSocket)
+
+        // close old socket when new one open to prevent duplicate messages
+        return () => newSocket.close()
+    }, [id])
+
     return (
-        <></>
+        <SocketContext.Provider value={socket} >
+            { children }
+        </SocketContext.Provider>
     )
 }
